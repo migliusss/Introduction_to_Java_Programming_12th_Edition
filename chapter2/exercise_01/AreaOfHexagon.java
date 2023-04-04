4b00261070142c26e7c6cca6c5a2bec7c4ef24f7
Migle <115370256+migliusss@users.noreply.github.com>
2023-04-04 20:00:22 +0200

    Create AreaOfHexagon.java

---
+++ chapter2/exercise_01/AreaOfHexagon.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+
+public class AreaOfHexagon {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the length of this side: ");
+		double sideLength = input.nextDouble();
+		
+		double area = (3 * Math.sqrt(3) / 2) * Math.pow(sideLength, 2);
+		
+		System.out.println("The area of the hexagon is " + area);
+	}
+}
