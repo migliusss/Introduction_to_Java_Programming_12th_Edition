45a735352f38b3ee561d43f98119464ac34c47a0
Migle <115370256+migliusss@users.noreply.github.com>
2023-04-06 14:20:48 +0200

    Update PassOrFail.java

--- chapter_05/exercise_01/PassOrFail.java
+++ chapter_05/exercise_01/PassOrFail.java
@@ -1,4 +1,4 @@
-/** 
+/* 
  * Write a program that prompts a student to enter a Java score. 
  * If the score is greater or equal to 60, display "you pass the exam"; 
  * otherwise, display "you don't pass the exam". 
