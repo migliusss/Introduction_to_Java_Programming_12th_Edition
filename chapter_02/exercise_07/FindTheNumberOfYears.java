9ca6a5838d3a490730b3683130b9312591ad9c67
Migle <115370256+migliusss@users.noreply.github.com>
2023-04-07 21:48:55 +0200

    Create FindTheNumberOfYears.java

---
+++ chapter_02/exercise_07/FindTheNumberOfYears.java
@@ -0,0 +1,37 @@
+package chapt2;
+
+/*
+ * Write a program that prompts the user to enter the minutes 
+ * and displays the maximum number of years and remaining days for the minutes.
+ * A year has 365 days. 
+ */
+
+import java.util.Scanner; 
+
+public class FindTheNumberOfYears {
+	public static void main(String[] args) {
+		// Create a Scanner 
+		Scanner input = new Scanner(System.in);
+		
+		// Prompt the user to enter the minutes.
+		System.out.print("Enter the number of minutes: ");
+		int minutes = input.nextInt();
+		
+		// Calculate hours.
+		int hours = minutes / 60; 
+		
+		// Calculate days.
+		int days = hours / 24; 
+		
+		// Calculate years.
+		int years = days / 365; 
+		
+		// Calculate remaining days.
+		int daysLeft = days % 365;
+		
+		// Display results.
+		System.out.println(minutes + " minutes is approximately " + years + " years and "
+				+ daysLeft + " days.");
+
+	}
+}
