8aaf491fb5e9f3decc1139c96f0f8808a08e42e8
Migle <115370256+migliusss@users.noreply.github.com>
2023-04-15 20:09:37 +0200

    Update ConversionBetweenPoundsKilograms.java

--- chapter_06/exercise_09/ConversionBetweenPoundsKilograms.java
+++ chapter_06/exercise_09/ConversionBetweenPoundsKilograms.java
@@ -1,1 +1,40 @@
+package chapt6;
 
+/*
+ * Write a class that contains the following two methods: 
+ * public static double poundToKilogram(double pound)
+ * public static double kilogramToPound(double kilogram)
+ * 
+ * The formula for the conversion is: 
+ * pound = 0.453 * kilogram
+ * kilogram = 2.204 * pound
+ * 
+ * Write a test program that invokes these methods to displays two tables. 
+ */
+
+public class ConversionBetweenPoundsKilograms {
+	public static void main(String[] args) {
+		// Print header. 
+		System.out.println("Kilograms    Pounds    |    Pounds    Kilograms");
+		System.out.println("-----------------------------------------------");
+		
+		// A for loop to print a two tables. 
+		for (double i = 1, j = 20; i <= 199 && j <= 515; i += 2, j += 5) {
+			System.out.printf("%-10.0f%7.1f      |    %-10.0f%.2f \n", i, poundToKilogram(i), j, kilogramToPound(j));
+		}
+	}
+	
+	// Convert from pounds to kilograms. 
+	public static double poundToKilogram(double pound) {
+		double kilograms = 2.204 * pound;
+		
+		return kilograms;
+		
+	}
+	// Convert from kilograms to pounds. 
+	public static double kilogramToPound(double kilogram) {
+		double pounds = 0.453 * kilogram;
+		
+		return pounds;
+	}
+}
