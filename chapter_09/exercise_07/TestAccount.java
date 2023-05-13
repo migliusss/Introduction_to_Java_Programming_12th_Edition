79d2a82aa34ed7b8fcd2e9d7f691e4db21fbe27b
Migle <115370256+migliusss@users.noreply.github.com>
2023-05-13 14:44:33 +0200

    Create TestAccount.java

---
+++ chapter_09/exercise_07/TestAccount.java
@@ -0,0 +1,30 @@
+package chapt9;
+
+public class TestAccount {
+	// Main method.
+	public static void main(String[] args) {
+		// Create an Account object. 
+		Account account = new Account(1122, 20000);
+		
+		// Set annual interest rate to 4,5%.
+		account.setAnnualInterestRate(4.5);
+		
+		// Withdraw $2,500.
+		account.withdraw(2500);
+		
+		// Deposit $3,000.
+		account.deposit(3000);
+		
+		System.out.println("Account " + account.getId());
+		
+		// Print the date when this account was created. 
+		System.out.println("The account was created: " + account.getDateCreated()); 
+		
+		// Print balance.
+		System.out.println("Balance: $" + account.getBalance());
+		
+		// Print monthly interest. 
+		System.out.println("Monthly interest rate: $" + account.getMonthlyInterest());
+		
+	}
+}
