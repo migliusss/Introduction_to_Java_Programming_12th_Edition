89d795deba1b20af7e52c577ce325f30873022e4
Migle <115370256+migliusss@users.noreply.github.com>
2023-09-16 17:00:32 +0200

    Create Exercise_07_36.java

---
+++ chapter_07/exercise_36/Exercise_07_36.java
@@ -0,0 +1,12 @@
+package chapter_07;
+
+/*
+ * (Game: Eight Queens) The classic Eight Queens puzzle is to place eight queens on a 
+ * chessboard such that no two queens can attack each other (i.e., no two queens are on 
+ * the same row, same column, or same diagonal). The are many possible solutions. 
+ * Write a program that displays one such solution. 
+ */
+
+public class Exercise_07_36 {
+
+}
